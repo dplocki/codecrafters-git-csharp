c38e4182add872d33657b991aac7aa6805a8d344
dplocki <dawid.plocki@gmail.com>
2024-12-29 15:15:39 +0100

    refactor: move the code of the cat-file reader into the seperate file

---
+++ src/CatFileSubProgram.cs
@@ -0,0 +1,44 @@
+using System.IO.Compression;
+using System.Text;
+
+internal static class CatFileSubProgram
+{
+    public static void Run(string[] args)
+    {
+        if (args.Length < 1 ) {
+            Console.WriteLine("Please provide a sub-command.");
+            return;
+        }
+
+        if (args[0] != "-p") {
+            throw new ArgumentException($"Unknown sub-command {args[0]}");
+        }
+
+        if (args.Length < 2 ) {
+            Console.WriteLine("Please provide a blob hash.");
+            return;
+        }
+
+        var hash = args[1];
+        if (hash.Length != 40) {
+            throw new ArgumentException($"Unk");
+        }
+
+        var filePath = $".git/objects/{hash.Substring(0, 2)}/{hash.Substring(2)}";
+        using var compressedFileStream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        using var decompressStream = new ZLibStream(compressedFileStream, CompressionMode.Decompress);
+        using var binaryStream = new BinaryReader(decompressStream);
+
+        using var memoryStream = new MemoryStream();
+        decompressStream.CopyTo(memoryStream);
+        var data = memoryStream.ToArray();
+        var nullIndex = Array.IndexOf(data, (byte)0);
+        var header = Encoding.UTF8.GetString(data, 0, nullIndex);
+
+        var content = new byte[data.Length - nullIndex - 1];
+        Array.Copy(data, nullIndex + 1, content, 0, content.Length);
+
+        string contentString = Encoding.UTF8.GetString(content);
+        Console.Write(contentString);
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,3 @@
-using System.IO.Compression;
-using System.Text;
-
 if (args.Length < 1)
 {
     Console.WriteLine("Please provide a command.");
@@ -8,6 +5,7 @@ if (args.Length < 1)
 }
 
 var command = args[0];
+var localArgs = args.Skip(1).ToArray();
 
 if (command == "init")
 {
@@ -19,43 +17,7 @@ if (command == "init")
 }
 else if (command == "cat-file")
 {
-    var localArgs = args.Skip(1).ToArray();
-
-    if (localArgs.Count() < 1 ) {
-        Console.WriteLine("Please provide a sub-command.");
-        return;
-    }
-
-    if (localArgs[0] != "-p") {
-        throw new ArgumentException($"Unknown sub-command {localArgs[0]}");
-    }
-
-    if (localArgs.Length < 2 ) {
-        Console.WriteLine("Please provide a blob hash.");
-        return;
-    }
-
-    var hash = localArgs[1];
-    if (hash.Length != 40) {
-        throw new ArgumentException($"Unk");
-    }
-
-    var filePath = $".git/objects/{hash.Substring(0, 2)}/{hash.Substring(2)}";
-    using var compressedFileStream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-    using var decompressStream = new ZLibStream(compressedFileStream, CompressionMode.Decompress);
-    using var binaryStream = new BinaryReader(decompressStream);
-
-    using var memoryStream = new MemoryStream();
-    decompressStream.CopyTo(memoryStream);
-    var data = memoryStream.ToArray();
-    var nullIndex = Array.IndexOf(data, (byte)0);
-    var header = Encoding.UTF8.GetString(data, 0, nullIndex);
-
-    var content = new byte[data.Length - nullIndex - 1];
-    Array.Copy(data, nullIndex + 1, content, 0, content.Length);
-
-    string contentString = Encoding.UTF8.GetString(content);
-    Console.Write(contentString);
+    CatFileSubProgram.Run(localArgs);
 }
 else
 {
