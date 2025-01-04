ac1dbf6457fce989ef08e2a0df8877feaa78df3e
dplocki <dawid.plocki@gmail.com>
2025-01-04 09:51:19 +0100

    feat: introduce the hash object

---
+++ src/Hash.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+
+internal class Hash
+{
+    private readonly byte[] content;
+
+    private Hash(byte[] content)
+    {
+        if (content.Length != 20)
+        {
+            throw new ArgumentException("Hash must be 20 bytes long");
+        }
+
+        this.content = content;
+    }
+
+    public static Hash FromStream(Stream stream)
+    {
+        using var sha1 = SHA1.Create();
+
+        stream.Position = 0;
+        return new Hash(sha1.ComputeHash(stream));
+    }
+
+    public override string ToString() {
+        return Convert.ToHexStringLower(content);
+    }
+}
