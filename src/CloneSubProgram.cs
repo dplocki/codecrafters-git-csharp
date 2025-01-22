d0ad4d99f366c530d4a12ac7cbde4ea487b13647
dplocki <dawid.plocki@gmail.com>
2025-01-22 16:43:16 +0100

    feat(clone): create the base directory

--- src/CloneSubProgram.cs
+++ src/CloneSubProgram.cs
@@ -2,5 +2,12 @@ internal class CloneSubProgram
 {
     public static void Run(string link, string directoryPath)
     {
+        if (Directory.Exists(directoryPath))
+        {
+            Console.Error.WriteLine($"Path {directoryPath} already exist");
+            return;
+        }
+
+        Directory.CreateDirectory(directoryPath);
     }
 }
